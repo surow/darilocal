0246ef77482a599968d6fedc71199728b9b10354
surow <surolds@gmail.com>
2018-09-06 13:15:28 +0700

    firstCommit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	
+	
+	@RestController
+	public class HelloController {
+
+	    @RequestMapping("/")
+	    public String index() {
+	        return "Greetings from Spring Boot! <br/> by suro";
+	    }
+
+	}
+}
